ffb463a352dd8bede8683fbf45961b427caf3326
ascotlan <91165103+ascotlan@users.noreply.github.com>
2022-09-24 22:50:12 -0400

    added media queries for country details page

--- src/requests.js
+++ src/requests.js
@@ -1,12 +1,12 @@
 const getCountries = async () => {
-  const response = await fetch('https://restcountries.com/v3.1/all')
+  const response = await fetch("https://restcountries.com/v3.1/all");
 
-  if(response.status === 200){
-    const data = await response.json()
-    return data
-  }else {
-    throw new Error('Unable to fetch country data')
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error("Unable to fetch country data");
   }
-}
+};
 
-export {getCountries as default}
+export { getCountries as default };
